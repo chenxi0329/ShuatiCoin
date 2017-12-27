9e162668c8541a8eaeefeb0f4ec2714d408da1d6
Xi Chen <chenxi0329@gmail.com>
2017-12-27 10:03:50 -0500

    wrote first migration script

--- migrations/1514322567_shuati_coin_i_c_o.js
+++ migrations/1514322567_shuati_coin_i_c_o.js
@@ -1,3 +1,8 @@
-module.exports = function(deployer) {
-  // Use deployer to state migration tasks.
+const ShuatiCoinICO = artifacts.require('ShuatiCoinICO')
+module.exports = function(deployer, network, accounts) {
+    const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
+    const endTime = startTime + (60 * 60 * 24 * 7) 
+    const rate = new web3.BigNumber(1000)
+    const wallet = accounts[0]
+    deployer.deploy(ShuatiCoinICO, startTime, endTime, rate, wallet)
 };
