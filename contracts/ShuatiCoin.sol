6fb2dfa026d974d85b88d1a4db873b481919a600
Xi Chen <chenxi0329@gmail.com>
2017-12-27 09:44:22 -0500

    wrote first contracts

--- contracts/ShuatiCoin.sol
+++ contracts/ShuatiCoin.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.4;
+import '../zeppelin-solidity/contracts/token/MintableToken.sol';
 
-contract ShuatiCoin {
-  function ShuatiCoin() {
-    // constructor
-  }
+contract ShuatiCoin is MintableToken {
+    string public name = "Shuati Coin";
+    string public symbol = 'SHC';
+    uint8 public decimal = 10;
 }
--- contracts/ShuatiCoinICO.sol
+++ contracts/ShuatiCoinICO.sol
@@ -1,7 +1,14 @@
 pragma solidity ^0.4.4;
+import './ShuatiCoin.sol';
+import '../zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
-contract ShuatiCoinICO {
-  function ShuatiCoinICO() {
-    // constructor
-  }
+contract ShuatiCoinICO is Crowdsale {
+
+	function ShuatiCoinICO(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) Crowdsale(_startTime, _endTime, _rate, _wallet) public {          
+	}
+
+	function createTokenContract() internal returns(MintableToken) {
+		return new ShuatiCoin();
+	}
+
 }
